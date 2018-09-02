8b2f198ed88fa08f605c699ab27010ef9a34c6b9
Greger Olofsson <greger.olofsson@gmail.com>
2018-09-03 00:23:15 +0200

    wip candidatefilterstrategy

--- SodukuSolverConsole/Strategies/CandidateFilterStrategy.cs
+++ SodukuSolverConsole/Strategies/CandidateFilterStrategy.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace SudokuSolverConsole.Strategies
@@ -8,17 +9,28 @@ namespace SudokuSolverConsole.Strategies
         public override List<Square> TrySolve(IPlayingField field)
         {
             var modifiedSquares = new List<Square>();
+
+            modifiedSquares.AddRange(FilterCandidates(field.GetRow));
+            modifiedSquares.AddRange(FilterCandidates(field.GetColumn));
+            modifiedSquares.AddRange(FilterCandidates(field.GetBigSquare));
+
+            return modifiedSquares;
+        }
+
+        private static List<Square> FilterCandidates(Func<int,List<Square>> squareSelector)
+        {
+            var modifiedSquares = new List<Square>();
             for (var y = 0; y < PlayingField.Height; y++)
             {
-                var rowSquares = field.GetRow(y);
-                var usedNumbers = rowSquares
+                var currentSquares = squareSelector(y);
+                var usedNumbers = currentSquares
                     .Where(x => x.Value > 0)
                     .Select(square => square.Value)
                     .ToList();
 
                 foreach (var number in usedNumbers)
                 {
-                    foreach (var square in rowSquares.Where(x => x.Candidates != null))
+                    foreach (var square in currentSquares.Where(x => x.Candidates != null))
                     {
                         if (square.Candidates.Remove(number))
                             modifiedSquares.Add(square);
---
+++ SudokuSolverConsoleTestUnit/Strategies/CandidateFilterStrategyTests.cs
@@ -0,0 +1,155 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Moq;
+using SudokuSolverConsole;
+using SudokuSolverConsole.Strategies;
+using Xunit;
+
+namespace SudokuSolverConsoleTestUnit.Strategies
+{
+    public abstract class CandidateFilterStrategyTests
+    {
+        protected Mock<IPlayingField> _fieldMock = new Mock<IPlayingField>();
+        protected readonly List<Square> _testSquaresRow = new List<Square>();
+        protected readonly List<Square> _testSquaresCol = new List<Square>();
+        protected readonly List<Square> _testSquaresBig = new List<Square>();
+
+        protected CandidateFilterStrategy _strategy = new CandidateFilterStrategy();
+
+        protected CandidateFilterStrategyTests()
+        {
+            for (var i = 0; i < 9; i++)
+            {
+                _testSquaresRow.Add(new Square());
+            }
+
+            _testSquaresCol.Add(_testSquaresRow.First());
+            for (var i = 0; i < 8; i++)
+            {
+                _testSquaresCol.Add(new Square());
+            }
+
+            //rrr
+            //cnn
+            //cnn
+            _testSquaresBig.AddRange(_testSquaresRow.GetRange(0,3));
+            for (var r = 0; r < 2; r++)
+            {
+                _testSquaresBig.AddRange(_testSquaresRow.Skip(1+r).Take(1));
+                for (var i = 0; i < 2; i++)
+                {
+                    _testSquaresBig.Add(new Square());
+                }
+            }
+
+            _fieldMock.Setup(x => x.GetRow(It.IsAny<int>())).Returns(_testSquaresRow);
+            _fieldMock.Setup(x => x.GetColumn(It.IsAny<int>())).Returns(_testSquaresCol);
+            _fieldMock.Setup(x => x.GetBigSquare(It.IsAny<int>())).Returns(_testSquaresBig);
+        }
+
+        public class TrySolve : CandidateFilterStrategyTests
+        {
+            private static readonly List<int> CountNine = new List<int>{0,1,2,3,4,5,6,7,8};
+
+            [Fact]
+            public void Should_get_rows_from_field()
+            {
+                _strategy.TrySolve(_fieldMock.Object);
+
+                foreach (var i in CountNine)
+                {
+                    _fieldMock.Verify(x => x.GetRow(i), Times.Once);
+                }
+            }
+
+            [Fact]
+            public void Should_get_columns_from_field()
+            {
+                _strategy.TrySolve(_fieldMock.Object);
+
+                foreach (var i in CountNine)
+                {
+                    _fieldMock.Verify(x => x.GetColumn(i), Times.Once);
+                }
+            }
+
+            [Fact]
+            public void Should_get_bigsquare_from_field()
+            {
+                _strategy.TrySolve(_fieldMock.Object);
+
+                foreach (var i in CountNine)
+                {
+                    _fieldMock.Verify(x => x.GetBigSquare(i), Times.Once);
+                }
+            }
+
+            [Fact]
+            public void Should_remove_set_values_from_cadidate_lists_in_row()
+            {
+                _testSquaresRow[0].Value = 1;
+                _testSquaresRow[5].Value = 3;
+
+                _strategy.TrySolve(_fieldMock.Object);
+
+                foreach (var square in _testSquaresRow)
+                {
+                    Assert.DoesNotContain(1, square.Candidates);
+                    Assert.DoesNotContain(3, square.Candidates);
+                }
+            }
+
+            [Fact]
+            public void Should_remove_set_values_from_cadidate_lists_in_column()
+            {
+                _testSquaresRow[0].Value = 1;
+                _testSquaresCol[1].Value = 3;
+
+                _strategy.TrySolve(_fieldMock.Object);
+
+                foreach (var square in _testSquaresCol)
+                {
+                    Assert.DoesNotContain(1, square.Candidates);
+                    Assert.DoesNotContain(3, square.Candidates);
+                }
+            }
+
+            [Fact]
+            public void Should_remove_set_values_from_cadidate_lists_in_big_square()
+            {
+                _testSquaresRow[0].Value = 1;
+                _testSquaresCol[1].Value = 3;
+                _testSquaresBig[8].Value = 5;
+
+
+                _strategy.TrySolve(_fieldMock.Object);
+
+                foreach (var square in _testSquaresBig)
+                {
+                    Assert.DoesNotContain(5, square.Candidates);
+                }
+            }
+
+            [Fact]
+            public void Should_return_squares_have_that_have_had_candidates_removed()
+            {
+                _testSquaresRow[0].Value = 1;
+
+                var result = _strategy.TrySolve(_fieldMock.Object);
+
+                //Assert.True(result);
+            }
+
+            [Fact]
+            public void Should_return_false_when_squares_have_not_been_modified()
+            {
+                _testSquaresRow[0].Value = 1;
+
+                var firstTry = _strategy.TrySolve(_fieldMock.Object);
+                var secondTry = _strategy.TrySolve(_fieldMock.Object);
+
+                //Assert.False(secondTry);
+            }
+        }
+    }
+}
